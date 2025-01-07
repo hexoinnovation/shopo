df41d935774d62360db3fd459d1b9d7a16eb2ae7
Nithyavs <hexoinnovation@gmail.com>
2025-01-07 15:26:55 -0800

    Nithya

--- src/components/Partials/Headers/HeaderTwo/Navbar.jsx
+++ src/components/Partials/Headers/HeaderTwo/Navbar.jsx
@@ -988,7 +988,39 @@ export default function Navbar({ className }) {
             </div>
             <div className="become-seller-btn  w-[161px] h-[40px]">
               <Link to="/become-saller">
-                <div className="yellow-btn flex justify-center items-center cursor-pointer">
+                <div className="yellow-btn ml-14 flex justify-center items-center cursor-pointer">
+                  <div className="flex space-x-2 items-center">
+                    <span className="text-sm font-600">Login</span>
+                    <span>
+                      <svg
+                        width="6"
+                        height="10"
+                        viewBox="0 0 6 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <rect
+                          x="1.08984"
+                          width="6.94106"
+                          height="1.54246"
+                          transform="rotate(45 1.08984 0)"
+                        />
+                        <rect
+                          x="6"
+                          y="4.9082"
+                          width="6.94106"
+                          height="1.54246"
+                          transform="rotate(135 6 4.9082)"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="become-seller-btn  w-[161px] h-[40px]">
+              <Link to="/become-saller">
+                <div className="yellow-btn ml-20 flex justify-center items-center cursor-pointer">
                   <div className="flex space-x-2 items-center">
                     <span className="text-sm font-600">Become a Seller</span>
                     <span>
